945581f9e0a5955ff1add3adb494706192a1b9a2
MomFriendsSon <s.v.pukha@gmail.com>
2021-12-31 14:00:35 +0200

    the last component began

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 export default class EditExercise extends Component {
+  constructor(props) {
+    super(props);
+    this.onChageUsername = this.onChageUsername.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+  //insert last code here!!!!!!!!!!!!!!!!!!!!!!!!!
   render() {
     return <h2>This is create edit exercise</h2>;
   }
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+// component for making a table of exercises
+const Exercise = (props) => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise._id}>Edit</Link> |{" "}
+      {/*links for edit and delete */}
+      <a href="#" onClick={() => props.deleteExercise(props.exercise._id)}>
+        Delete
+      </a>
+    </td>
+  </tr>
+);
+
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
@@ -18,7 +35,45 @@ export default class ExercisesList extends Component {
       .catch((error) => console.log(error.message));
   }
 
+  deleteExercise(id) {
+    axios
+      .delete("http://localhost:5000/exercises/" + id)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error.message));
+    this.setState({
+      exercises: this.state.exercises.filter((el) => el._id !== id), // if _id=id element deleted from state array and update the page
+    });
+  }
+  exerciseList() {
+    return this.state.exercises.map((curExercise) => {
+      //return functional component in (bracers) with props
+      return (
+        <Exercise
+          exercise={curExercise}
+          deleteExercise={this.deleteExercise}
+          key={curExercise._id}
+        />
+      );
+    });
+  }
+
   render() {
-    return <h2>This is exercises list</h2>;
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
+      </div>
+    );
   }
 }
