3dcddce782c4c4b91e49a4891ef57f5b4af7984d
MomFriendsSon <s.v.pukha@gmail.com>
2021-12-31 14:10:11 +0200

    little piece of code

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -20,6 +20,20 @@ export default class EditExercise extends Component {
       users: [],
     };
   }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date),
+        });
+      })
+      .catch((e) => console.log(e.message));
+  }
+
   //insert last code here!!!!!!!!!!!!!!!!!!!!!!!!!
   render() {
     return <h2>This is create edit exercise</h2>;
