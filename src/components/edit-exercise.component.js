fa6f16ab99e4d61f2a3da1761f6bfd30235010ee
MomFriendsSon <s.v.pukha@gmail.com>
2022-01-04 15:54:32 +0200

    some error in params need to be debugged dont know how for now

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -6,8 +6,9 @@ import axios from "axios";
 export default class EditExercise extends Component {
   constructor(props) {
     super(props);
+
     this.onChageUsername = this.onChageUsername.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
     this.onChangeDuration = this.onChangeDuration.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -32,10 +33,110 @@ export default class EditExercise extends Component {
         });
       })
       .catch((e) => console.log(e.message));
+    axios
+      .get("http://localhost:5000/users/")
+      .then((res) => {
+        this.setState({ users: res.data.map((user) => user.username) });
+      })
+      .catch((e) => console.log(e.message));
+  }
+  onChageUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+  onChangeDuration(e) {
+    this.setSstate({
+      duration: e.target.value,
+    });
+  }
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+  onSubmit(e) {
+    e.preventDafault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+    console.log(exercise);
+    axios
+      .post(
+        "http://localhost:5000/exercises/update/" + this.props.match.params.id,
+        exercise
+      )
+      .then((res) => console.log(res.data));
+    window.location("/");
   }
 
   //insert last code here!!!!!!!!!!!!!!!!!!!!!!!!!
   render() {
-    return <h2>This is create edit exercise</h2>;
+    return (
+      <div>
+        <h3>Edit Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChageUsername}
+            >
+              {this.state.users.map(function (user) {
+                return (
+                  <option value={user} key={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date:</label>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
